153756cbc3eda4c885d8a96999bfa4133807bc0b
Your Name <you@example.com>
2022-08-07 20:28:39 -0300

    Primitive and Non Primitive

---
+++ types/primitive-nonPrimitive.js
@@ -0,0 +1,25 @@
+//  Primitives
+//  Can only save one data, the value, and does not change line an object and array, but you can reasing it
+//  Example
+let car = "car";
+console.log(car); //  car
+car.toUpperCase();
+console.log(car); //  car (No change)
+car[0] = "b";
+console.log(car); //  car (No change)
+
+//  reasing it
+car = car.toUpperCase();
+console.log(car); //    CAR
+
+//  Primitives
+//  - Null
+//  - Undefined
+//  - Boolean
+//  - Number
+//  - Bigint
+//  - String
+//  - Symbol
+
+//  Non Primites
+//  Object
